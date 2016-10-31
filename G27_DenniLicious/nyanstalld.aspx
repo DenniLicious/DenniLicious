12fa4b46942c5dedee3559eaef6b8fc9b02929c9
lisettes <lisette_svedberg@hotmail.com>
2016-10-31 13:09:51 +0100

    nyanstalld.aspx

---
+++ G27_DenniLicious/nyanstalld.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Licensieringstest" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="nyanstalld.aspx.cs" Inherits="G27_DenniLicious.nyanstalld" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id="allafragor" runat="server"></div>
+
+</asp:Content>
+
+
---
+++ G27_DenniLicious/nyanstalld.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace G27_DenniLicious
+{
+    public partial class nyanstalld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string path = Server.MapPath("fragor.xml");
+            XmlSwitchCase();
+        }
+
+        public void XmlSwitchCase()
+        {
+            string fragor = "";
+            string path = Server.MapPath("fragor.xml");
+            XmlTextReader xreader = new XmlTextReader(path);
+
+            //läser igenom Xml-filen
+            while (xreader.Read())
+            {
+                switch (xreader.Name)
+                {
+                    case "text":
+                        fragor += xreader.ReadString();
+                        break;
+                }
+            }
+
+            allafragor.InnerHtml = fragor;
+        }
+    }
+}
