6493cd8b4123f0f9022efca75c5ec75f34da8adf
lisettes <lisette_svedberg@hotmail.com>
2016-11-01 12:56:21 +0100

    Class: Anvandare - lagt till properties

---
+++ G27_DenniLicious/Anvandare.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace G27_DenniLicious
+{
+    public class Anvandare
+    {
+        public int Id { get; set; }
+        public string Namn { get; set; }
+        public string Typ { get; set; }
+    }
+}
